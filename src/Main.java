c7334c73fee8d2695629b260a8ded338e8809bc4
cassiocoltri <cassiocoltri@gmail.com>
2024-10-07 19:28:44 -0300

    Testando a conexão com o servidor da Exchance API.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Grandão!!");
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        final String apiKey = "2142276e6e85e4b9de64d30f";
+
+        String addres = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/USD";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(addres))
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Moeda não encontrada...");
+        }
+
+        String json = response.body();
+
+        System.out.println(json);
+
+        scanner.close();
 
     }
 }
